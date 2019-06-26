86c76722333681086fc2dbc08a79600ad6c97fd6
masongabe <mason@tworoadsacademy.com>
2019-06-26 07:38:14 -0400

    add basic canvas stuff

--- games/move/js/move.js
+++ games/move/js/move.js
@@ -1,1 +1,25 @@
-console.log('MOVE IT MOVE IT');
+
+var canvasElement = document.getElementById('game-canvas');
+var canvas = canvasElement.getContext('2d');
+
+
+var FPS = 30;
+setInterval(function() {
+    update();
+    draw();
+}, 1000/FPS);
+
+var textX = 50;
+var textY = 50;
+
+
+function update() {
+    textX += 1;
+    textY += 1;
+}
+
+function draw() {
+    canvas.clearRect(0, 0, 640, 480);
+    canvas.fillStyle = "#000";
+    canvas.fillText("Sup Bro!", textX, textY);
+}
