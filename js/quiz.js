1a16e7bbf469d09662fe99ce703a43921e6abcd8
Mason King <mason@tworoadsacademy.com>
2020-07-01 12:26:58 -0400

    Quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,13 +1,43 @@
 (function($){
 
     $("#done").on('click', function(){
-        var option = $("input[name=option]:checked").val();
+
+        var option1 = $("input[name=option1]:checked").val();
+
             var right = 0;
             var wrong = 0;
-            var total = 1;
-            if(option == 'wrong'){
-                wrong++
-            }else if(option == 'true'){
+            var total = 4;
+            if(option1 == 'false'){
+                $("#Q1").addClass("wrong").removeClass("right");
+            }else if(option1 == 'true'){
+                $("#Q1").addClass("right").removeClass("wrong");
+                right++
+            }
+        var option2 = $("input[name=option2]:checked").val();
+
+
+            if(option2 == 'false'){
+                $("#Q2").addClass("wrong").removeClass("right");
+            }else if(option2 == 'true'){
+                $("#Q2").addClass("right").removeClass("wrong");
+                right++
+            }
+
+            var option3 = $("input[name=option3]:checked").val();
+
+            if(option3 == 'false'){
+                $("#Q3").addClass("wrong").removeClass("right");
+            }else if(option3 == 'true'){
+                $("#Q3").addClass("right").removeClass("wrong");
+                right++
+            }
+
+            var option4 = $("input[name=option4]:checked").val();
+
+            if(option4 == 'false'){
+                $("#Q4").addClass("wrong").removeClass("right");
+            }else if(option4 == 'true'){
+                $("#Q4").addClass("right").removeClass("wrong");
                 right++
             }
 
@@ -20,3 +50,4 @@
 
 
 })(jQuery);
+
